2b95bb99b60d56455260a5138959571d8219afea
Razor <razorcd@yahoo.com>
2014-05-14 18:12:04 +0300

    mongo CR_D

---
+++ config/db.js
@@ -0,0 +1,91 @@
+var mongoose = require('mongoose');
+
+var dbSchema = new mongoose.Schema({
+		title: { type : String, default : "" },
+		text : { type : String, default : "" },
+		expire: { type : Date, default : 0 },
+		created : { type : Date, default : Date.now() }
+	});
+
+var NoteModel = mongoose.model("Note", dbSchema);
+
+//connect to DB
+exports.connectDb = function(user, pass){
+	//console.log(user,pass);
+	var DBurl = "mongodb://" + user + ":" + pass + "@ds033267.mongolab.com:33267/easynotes";
+	
+	mongoose.connect(DBurl);
+
+	// var Cat = mongoose.model('Cat', {name:String});
+	// var kitty = new Cat({name:'ZZZZ'});
+	// kitty.save(function(err){
+	// 	if (err) console.log(err);
+	// 	console.log("DONE");
+	// });
+
+}
+
+exports.addNote = function(note,cb){
+	var newNote = new NoteModel(note);
+
+	newNote.save(function(err){
+		if (err) { cb(err);	return;	}
+		cb(null);
+	})
+
+}
+
+
+
+exports.getAllNotes = function(cb){
+	var querry = NoteModel.find();
+
+	querry.exec(function(err, notes){
+		if (err) { cb(err); return; }
+		cb(null, notes);
+	})
+}
+
+
+exports.deleteById= function(id, cb){
+	NoteModel.findById(id, function(err, note){
+		if (err) { cb(err); return; }
+		if(note) {
+			foundNote = new NoteModel(note);
+			foundNote.remove(function(err, deletedNote){
+				if (err) { cb(err); return; }
+				cb(null, {deletedNote : deletedNote});
+			})
+		} else cb({error: "can't find ID"});
+	})
+}
+
+
+//searches titeText in all Ttitles
+exports.findByTitleSearch = function(titleText, cb){
+	var titleRegExp = new RegExp(titleText,"i");
+
+	var querry = NoteModel.find({title: titleRegExp});
+
+	querry.exec(function(err,notes){
+		if (err ) { cb(err); return; }
+		if (notes.length < 1) { cb({error: "Can't find TEXT in TITLES"}); return; }
+		cb(null, notes);
+	})
+
+}
+
+
+//searches exact Title
+exports.findByTitle = function(title, cb){
+	var titleRegExp = new RegExp("^(\\s*)(" + title + ")(\\s*)$","i");
+
+	var querry = NoteModel.find({title: titleRegExp});
+
+	querry.exec(function(err,notes){
+		if (err ) { cb(err); return; }
+		if (notes.length < 1) { cb({error: "Can't find TITLE"}); return; }
+		cb(null, notes);
+	})
+
+}
--- server.js
+++ server.js
@@ -1,6 +1,47 @@
-var express=require("express");
+var express = require("express");
+var db = require("./config/db.js");
 
 var serverApp = express();
+
+
+//var DBurl =process.env.DATABASE_URL || "postgres://djjtbgajpsbulz:XQYa1HvDXVMswCH2-BzJ-LuDsS@ec2-54-83-196-217.compute-1.amazonaws.com:5432/d33odevsd8p14f";
+
+db.connectDb("testUser", "test");
+ // db.addNote({
+ // 			title: "sdsssssssssss",
+ // 			text: "sdgdxxxxxxxxxxxxxxxxxxxxxxxfg",
+ // 			expire: Date.parse("2016")
+ // 			},
+ // 			 function(err){
+ // 				if (err) console.log(err);
+ // 			}
+ // );
+
+// db.getAllNotes(function(err,notes){
+//  	if (err) console.log(err);
+// 	console.log(notes);
+// });
+
+// db.deleteById("53737c69f39a37e03471805b", function(err,note){
+// 	if (err) { console.log(err); return; }
+// 	console.log("Deleted: ");
+// 	console.log(note.deletedNote);
+// })
+
+// db.findByTitleSearch("a", function(err, notes){
+// 	if (err) { console.log(err); return; }
+// 	console.log("Found my text in title: ");
+// 	console.log(notes);
+// })
+
+db.findByTitle("AAAAAAAAA", function(err, notes){
+	if (err) { console.log(err); return; }
+	console.log("Found by exact title: ");
+	console.log(notes);
+})
+
+
+
 
 serverApp.configure(function() {
 	serverApp.use(express.static(__dirname + "/public"));
